c11a32b75f31c36ce1842bac7f7b649f9951610c
shockValue666 <mapedeveloper@protonmail.com>
2024-03-01 17:16:22 -0800

    gang

--- src/app/(dash)/dashboard/page.tsx
+++ src/app/(dash)/dashboard/page.tsx
@@ -1,13 +1,4 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-//creates a supabase client for a server component: 
-//-a server component runs on the server-side
-//-it's executed on the server before any content is sent to the client
-// e.g. -when a user logs in on the client side, a cookie containing an authentication token might be set in the browser
-//-later, when a request is made on the server component, it can access the cookies using context.cookies to ckeck
-//if the user is authenticatied
-//-if he is authenticatd the serverside can perform some shit
-// import React, {useEffect} from 'react'
-
 import { cookies } from 'next/headers'
 import {db} from '@/lib/supabase/db'
 import { redirect } from 'next/navigation'
@@ -17,6 +8,7 @@ import { Button } from '@/components/ui/button'
 import DashboardSetup from '@/components/dashboard/dashboard-setup'
 
 const Dashboard = async () => {
+  cookies().getAll();
 
   const supabase  = createServerComponentClient({cookies})
   const {
