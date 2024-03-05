310259969c75b133ad481560f346bffb454c9929
shockValue666 <mapedeveloper@protonmail.com>
2024-03-05 15:22:35 -0800

    more

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -37,12 +37,14 @@ const page = () => {
       </div>
       <MainShit/>
       <div>
-        <a className="p-[3px] relative" href="/dashboard" target="_blank">
+        <button className="p-[3px] relative">
           <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
           <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-            Try it now!
+            <a href="/dashboard" target='_blank'>
+              Try it now!
+            </a>
           </div>
-        </a>
+        </button>
       </div>
       <div className='my-16'>
             <Image src={Banner} alt={'Application Banner'}/>
