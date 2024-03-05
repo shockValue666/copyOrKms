8ab74a88523e2ea22768a81f313794a9f5b5d96c
shockValue666 <mapedeveloper@protonmail.com>
2024-03-05 15:20:10 -0800

    anchor

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -23,12 +23,11 @@ import { useRouter } from 'next/navigation';
 
 
 const page = () => {
-  const router = useRouter()
+  // const router = useRouter()
 
   const handleClick = () => {
     // Handle the click event here
     console.log('Button clicked!');
-    router.replace(`/dashboard`)
   };
   
   return (
@@ -38,12 +37,12 @@ const page = () => {
       </div>
       <MainShit/>
       <div>
-        <button className="p-[3px] relative" onClick={handleClick}>
+        <a className="p-[3px] relative" href="/dashboard" target="_blank">
           <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
           <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
             Try it now!
           </div>
-        </button>
+        </a>
       </div>
       <div className='my-16'>
             <Image src={Banner} alt={'Application Banner'}/>
